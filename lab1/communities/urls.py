e1b0f3ccb11f0e07f4adda24171102985110d4af
ArTiKYARE <crahercenter@gmail.com>
2024-12-07 20:40:23 +0600

    lab2-finish

--- lab1/communities/urls.py
+++ lab1/communities/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'communities'
+
 urlpatterns = [
     path('', views.communities_list, name="communities"),
+    path('<slug:slug>/', views.communitie_page, name="communitie_page"),
 ]
--- lab1/communities/views.py
+++ lab1/communities/views.py
@@ -4,3 +4,7 @@ from .models import Communities
 def communities_list(request):
     communities = Communities.objects.all().order_by('-date')
     return render(request, 'communities/communities_list.html', {'communities': communities})
+
+def communitie_page(request, slug):
+    communitie = Communities.objects.get(slug=slug)  # исправлено имя переменной
+    return render(request, 'communities/communitie_page.html', {'communitie': communitie})
